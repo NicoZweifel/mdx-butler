f8c18ff935f4aa6aa001c8da6c4c457fe0d4a347
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-02-01 04:39:33 +0100

    save

--- docs/options.ts
+++ docs/options.ts
@@ -7,7 +7,7 @@ import { Options } from './services';
 import packageJsonFile from './package.json';
 
 const options: Options = {
-  name: packageJsonFile.name,
+  name: 'mdx-tug',
   repository: packageJsonFile.repository,
   license: packageJsonFile.license,
   author: packageJsonFile.author,
