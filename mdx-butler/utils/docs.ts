4cd1fa8eaa52ed9624c8f4018c9579ff0c8d9cc2
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-02-18 08:45:22 +0100

    chore: format

--- mdx-butler/utils/docs.ts
+++ mdx-butler/utils/docs.ts
@@ -5,7 +5,7 @@ import {
   MDXBundlerServiceOptions,
 } from '../types/index.js';
 
-export function docs<
+export const docs = <
   TFrontmatter extends Partial<Record<keyof TFields, string>>,
   TOptions extends
     MDXBundlerServiceBaseOptions<TFrontmatter> = MDXBundlerServiceBaseOptions<TFrontmatter>,
@@ -13,10 +13,6 @@ export function docs<
     TFrontmatter,
     TOptions
   >,
->(options?: MDXBundlerServiceOptions<TFrontmatter, TOptions, TFields>) {
-  const service = MDXBundlerService.create<TFrontmatter, TOptions, TFields>(
-    options
-  );
-
-  return service.docs();
-}
+>(
+  options?: MDXBundlerServiceOptions<TFrontmatter, TOptions, TFields>
+) => MDXBundlerService.create<TFrontmatter, TOptions, TFields>(options).docs();
