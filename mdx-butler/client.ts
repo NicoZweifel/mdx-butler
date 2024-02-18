dad5ef3268b2a8251fd4fd98b8145cfa60c0dd17
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-02-18 08:20:10 +0100

    v0.5.2

--- mdx-butler/client.ts
+++ mdx-butler/client.ts
@@ -2,5 +2,5 @@ import { getMDXComponent, getMDXExport } from 'mdx-bundler/client/index.js';
 import {
   ReactComponent as Component,
   useComponent,
-} from './components/ReactComponent.js';
+} from './components/ReactComponent';
 export { getMDXComponent, getMDXExport, useComponent, Component };
