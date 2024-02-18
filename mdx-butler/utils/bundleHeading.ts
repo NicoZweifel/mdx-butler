008330b76b85fa01536cc7b67bbc1153b6b2b9fb
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-02-18 09:09:46 +0100

    chore: format

--- mdx-butler/utils/bundleHeading.ts
+++ mdx-butler/utils/bundleHeading.ts
@@ -1,13 +1,11 @@
 import { bundleMDX } from 'mdx-bundler';
 import { DocHeading } from '../types/index.js';
 
-export async function bundleHeading(heading: DocHeading) {
-  return {
-    ...heading,
-    content: (
-      await bundleMDX({
-        source: heading.title,
-      })
-    ).code,
-  };
-}
+export const bundleHeading = async (heading: DocHeading) => ({
+  ...heading,
+  content: (
+    await bundleMDX({
+      source: heading.title,
+    })
+  ).code,
+});
--- mdx-butler/utils/bundleHeadings.ts
+++ mdx-butler/utils/bundleHeadings.ts
@@ -1,6 +1,5 @@
 import { bundleHeading } from './bundleHeading.js';
 import { DocHeading } from '../types/index.js';
 
-export async function bundleHeadings(headings: DocHeading[]) {
-  return Promise.all(headings.map(bundleHeading));
-}
+export const bundleHeadings = async (headings: DocHeading[]) =>
+  Promise.all(headings.map(bundleHeading));
