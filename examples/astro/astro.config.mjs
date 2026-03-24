916e699d92f178cfc20117318c48fb2a25f10f6f
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-03-24 21:49:08 +0100

    add astro example

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [react()],
   vite: {
     ssr: {
       // This helps during development in a monorepo
-      noExternal: ['mdx-butler']
-    }
-  }
+      noExternal: ["mdx-butler"],
+    },
+  },
 });
-
