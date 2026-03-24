1d06506907112a009063ab9e2a60927ebe227f6f
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-03-24 23:04:39 +0100

    Update astro.config.mjs

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   vite: {
     ssr: {
       // This helps during development in a monorepo
+      // You don't need this
       noExternal: ["mdx-butler"],
     },
   },
